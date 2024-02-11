38e31d226cf7d985430dfabf5403c8a461e6c109
Anton Panov <pan300503@gmail.com>
2024-02-11 17:48:52 +0300

    Added antlr dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    antlr
+    idea
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+val antlr4 = "org.antlr:antlr4"
+val antlrVersion = "4.9.3"
 
 repositories {
     mavenCentral()
@@ -11,11 +16,25 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // antlr
+    antlr("$antlr4:$antlrVersion")
+    compileOnly("$antlr4-runtime:$antlrVersion")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.compileKotlin {
+    dependsOn("generateGrammarSource")
+}
+
+tasks.generateGrammarSource {
+    maxHeapSize = "128m"
+    arguments = arguments + listOf("-visitor", "-listener")
+}
+
 kotlin {
     jvmToolchain(21)
 }
