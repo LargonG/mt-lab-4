f177f8a5b5d3a87f5670841d6b697f0b9df875ea
Anton Panov <pan300503@gmail.com>
2024-02-18 02:56:42 +0300

    Bug fix with rules arguments * Deleted ApplyArg type class

--- src/main/kotlin/org/largong/clarity/grammar/scripts/Script.kt
+++ src/main/kotlin/org/largong/clarity/grammar/scripts/Script.kt
@@ -4,4 +4,3 @@ data class Script(val inner: String)
 
 data class Declaration(val args: List<Arg>, val returns: List<Arg>)
 data class Arg(val name: String, val type: String)
-data class ApplyArg(val name: String)
